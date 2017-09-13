64a9eae3e35ba00c8f12d1cf1b83f826d7ac8671
Manuel Tancoigne <m.tancoigne@gmail.com>
2017-09-13 17:14:45 +0200

    Routes for post

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('/posts', 'PostsController@index')->name('posts.index');
+Route::get('/posts/new', 'PostsController@create')->name('posts.create');
+Route::post('/posts/store', 'PostsController@store')->name('posts.store');
+Route::get('/posts/show/{id}', 'PostsController@show')->name('posts.show');
+Route::get('/posts/edit/{id}', 'PostsController@edit')->name('posts.edit');
+Route::post('/posts/update/{id}', 'PostsController@update')->name('posts.update');
+Route::get('/posts/destroy/{id}', 'PostsController@destroy')->name('posts.destroy');
